1471716540f19478b5eff00ebe4402328d3b1c5d
Melkam Eziokwu <melkamev@gmail.com>
2025-09-15 18:04:21 -0500

    chore: added test setup

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	test: {
+		globals: true,
+		environment: 'jsdom', // simulates browser environment
+		setupFiles: './src/setupTests.ts'
+	},
 })
